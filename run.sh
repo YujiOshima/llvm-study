51d0d92986d1ed18690b4ddf8620076ea6d128c2
YujiOshima <yuji.oshima0x3fd@gmail.com>
2020-07-28 16:56:01 +0900

    add run on OSX
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- run.sh
+++ run.sh
@@ -2,4 +2,13 @@ set -ex
 docker build -f Dockerfile.llvmlite -t llvm-lite .
 docker run -it --rm -v $PWD/front:/front llvm-lite python main.py
 docker build -f Dockerfile.runtime -t llvm-runtime .
+set +e
+echo "run on Linux"
 docker run -it --rm llvm-runtime ./run_main
+docker run --name tmp-llvm -itd llvm-runtime sleep 10
+docker cp tmp-llvm:/front/main.s .
+docker rm -f tmp-llvm
+clang main.s -o run_main
+echo "run on OSX"
+./run_main
+rm run_main main.s
